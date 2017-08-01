3715fcc86ea340d4c6189f221758d7032f527dd6
Bassrobbe <robert.schaedlich@web.de>
2017-08-01 18:21:14 +0200

    encode URL now with java.net.URLEncoder instead of user defined Map

--- src/main/scala/Redirector/Redirector.scala
+++ src/main/scala/Redirector/Redirector.scala
@@ -1,6 +1,7 @@
 package Redirector
 
 import java.io.File
+import java.net.URLEncoder
 
 import org.scalatra._
 
@@ -24,49 +25,52 @@ class Redirector extends MmoonredirectorStack {
     val schema = params("schema")
     val res = params("res")
 
-    val iri = repl(s"http://mmoon.org/$lang/inventory/$schema/$res")
-    val sparql = repl("http://mmoon.org/sparql/")
+    val iri = URLEncoder.encode(s"http://mmoon.org/$lang/inventory/$schema/$res", "UTF-8")
+    val sparql = URLEncoder.encode("http://mmoon.org/sparql/", "UTF-8")
 
     redirect("http://lodview.it/lodview/?IRI=" + iri + "&sparql=" + sparql)
   }
 
-  //serve complete core, schema and inventory files
-  get("""^((/core|/[a-z]*/(schema|inventory)/[a-z]*)(.ttl|.html|.rdf|.owx|.omn|.ofn|.nt|/?))$""".r) {
-    //try content negotiation if no type is given via URI
-    if (multiParams("captures").apply(3).matches("/?")) {
-      //TODO
-    } else { //ignore "Accept" header if file extension is given via URI
-      findFile(documentRoot + multiParams("captures").apply(0)) match {
-        case Some(file) => Ok(file, Map("Content-Type" -> getConTypeByFileExt(multiParams("captures")
-          .lift(3)).getOrElse("")))
-        case None => NotFound("Sorry, the file could not be found")
-      }
-    }
-  }
-
-  //sparql query redirects
-  get("""^/sparql""") {  //TODO: add a guard/check s.t. this redirect only triggers if a 'query' param is present
-    redirect("http://localhost:9999/blazegraph/namespace/mmoon/sparql?query=" + params("query"))
-  }
-  post("""^/sparql""") {
-    redirect("http://localhost:9999/blazegraph/namespace/mmoon/sparql?query=" + params("query"))
-  }
-
-  //TODO: use java.net.URLEncoder#encode(String, String) with UTF 8 instead! rename to 'urlencode'
-  private def repl(str: String): String = {
-    return Map(":" -> "%3A", "/" -> "%2F").foldLeft(str) { case (str: String, (x, y)) => str.replaceAll(x, y) }
-  }
-
-  private def findFile(path: String): Option[File] = {
-    val file = new File(path)
-    if (file.exists && !file.isDirectory) Some(file) else None
-  }
-
-  private def getConTypeByFileExt(fileExt: Option[String]): Option[String] = {
-    contentTypeMapping.toMap.get(fileExt.getOrElse(""))
-  }
-
-  private def getFileExtByConType(contentType: String) : Option[String] = {
-    contentTypeMapping.map(x => x.swap).toMap.get(contentType)
-  }
+//  //serve complete core, schema and inventory files
+//  get("""^((/core|/[a-z]*/(schema|inventory)/[a-z]*)(.ttl|.html|.rdf|.owx|.omn|.ofn|.nt|/?))$""".r) {
+//    //try content negotiation if no type is given via URI
+//    if (multiParams("captures").apply(3).matches("/?")) {
+//      //TODO
+//    } else { //ignore "Accept" header if file extension is given via URI
+//      findFile(documentRoot + multiParams("captures").apply(0)) match {
+//        case Some(file) => Ok(file, Map("Content-Type" -> getConTypeByFileExt(multiParams("captures")
+//          .lift(3)).getOrElse("")))
+//        case None => NotFound("Sorry, the file could not be found")
+//      }
+//    }
+//  }
+//
+//  //sparql query redirects
+//  get("""^/sparql""".r) {
+//    if (params.contains("query"))
+//      redirect("http://localhost:9999/blazegraph/namespace/mmoon/sparql?query=" + params("query"))
+//    else
+//      <p>Sorry, query is empty</p>
+//  }
+//
+//  post("""^/sparql""".r) {
+//    if (params.contains("query"))
+//      redirect("http://localhost:9999/blazegraph/namespace/mmoon/sparql?query=" + params("query"))
+//    else
+//      <p>Sorry, query is empty</p>
+//  }
+//
+//  private def findFile(path: String): Option[File] = {
+//    val file = new File(path)
+//    if (file.exists && !file.isDirectory) Some(file) else None
+//  }
+//
+//  private def getConTypeByFileExt(fileExt: Option[String]): Option[String] = {
+//    contentTypeMapping.toMap.get(fileExt.getOrElse(""))
+//  }
+//
+//  private def getFileExtByConType(contentType: String) : Option[String] = {
+//    contentTypeMapping.map(x => x.swap).toMap.get(contentType)
+//  }
+  
 }
