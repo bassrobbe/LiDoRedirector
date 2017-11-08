5697e7379497bd6c1aa89163bf8be3fe0c8ea15f
Bassrobbe <robert.schaedlich@web.de>
2017-11-08 20:56:19 +0100

    update tests

--- src/test/scala/org/mmoon/scalatra/RedirectorTest.scala
+++ src/test/scala/org/mmoon/scalatra/RedirectorTest.scala
@@ -19,7 +19,10 @@ class RedirectorTest extends FunSuite {
     ("text/html","text/html"),
     ("fsd/sdf,text/html;q=0.9","text/html"),
     ("image/webp,application/rdf+xml;q=0.9,application/*;q=0.8,*/*;q=0.8","application/rdf+xml"),
-    ("text/html;q=0.9,application/rdf+xml;q=0.9,image/webp,application/*;q=0.8,*/*;q=0.8","text/html")
+    ("text/html;q=0.9,application/rdf+xml;q=0.9,image/webp,application/*;q=0.8,*/*;q=0.8","text/html"),
+    ("text/*","text/"),
+    ("*/*","")
+
   )
 
   val supportedFileExtenisons = List(".rdf", ".ttl", ".nt", ".html")
@@ -33,21 +36,50 @@ class RedirectorTest extends FunSuite {
     "http://mmoon.org/deu/inventory/og/DerivedWord_verkaufen"
   )
 
-  //val unsupported
+  val unsupportedMimeTypes = List(
+    ("application/javascript"),
+    ("application/json"),
+    ("text/css"),
+    ("image/jpeg")
+  )
+
+  val nonExistentUris = List(
+    "http://mmoon.org/fr/schema/of",
+    "http://mmoon.org/fr/schema/of/Affix",
+    "http://mmoon.org/fr/inventory/of",
+    "http://mmoon.org/fr/inventory/of/test",
+    "http://mmoon.org/deu/inventory/og/DerivedWord_kaufen",
+    "http://mmoon.org/fr/schema/of.html",
+    "http://mmoon.org/fr/schema/of/Affix.ttl",
+    "http://mmoon.org/fr/inventory/of.nt",
+    "http://mmoon.org/fr/inventory/of/test.rdf",
+    "http://mmoon.org/deu/inventory/og/DerivedWord_kaufen.html"
+  )
 
 
   test("URIs without file extension -> Content Negotiation") {
 
     for (acceptHeader <- supportedAcceptHeaders; uri <- "http://mmoon.org/core/" :: supportedUris) {
-      println(uri)
-      println(acceptHeader)
+
       val response: HttpResponse[String] = Http(uri).header("Accept", acceptHeader._1).asString
 
       assert(response.code == 303)
       assert(response.contentType.fold(false) { _.startsWith(acceptHeader._2) })
 
-      println(s"URI: ${uri}     Accept: ${acceptHeader._1}     Identified MimeType: ${acceptHeader._2}     Status: succesful")
+      val format = acceptHeader._2 match {
+
+//        case "application/rdf+xml" => Some(RDFFormat.RDFXML) //it seems rdf+xml could not be parsed correctly
+        case "text/turtle" => Some(RDFFormat.TURTLE)
+        case "application/n=triples" => Some(RDFFormat.NTRIPLES)
+        case _ => None
+      }
+
+      checkFormat(format, response)
+
+      println(s"URI: ${uri}     Accept: ${acceptHeader._1}     Identified MimeType: ${response.contentType.get}     Status: succesful")
     }
+
+
 
   }
 
@@ -69,23 +101,53 @@ class RedirectorTest extends FunSuite {
         case _ => None
       }
 
-      val rdfData : InputStream = new ByteArrayInputStream(response.body.getBytes)
-
-
-      format.fold() {
-
-        format => {
-
-          try { Rio.parse(rdfData, "", format) }
-
-          catch { case e : Exception => assert(false) }
-        }
-      }
+      checkFormat(format, response)
 
       println(s"URI: ${uri}${fileExt}     Status: succesful")
 
     }
 
+  }
+
+  private def checkFormat(format : Option[RDFFormat], response : HttpResponse[String]) = {
+
+    val rdfData : InputStream = new ByteArrayInputStream(response.body.getBytes)
+
+    format.map {
+
+      format => {
+
+        try { Rio.parse(rdfData, "", format) }
+
+        catch { case e : Exception => assert(false) }
+      }
+    }
+
+
+
+  }
+
+  test("unsupported Mimetypes -> 415") {
+    for (t <- unsupportedMimeTypes; uri <- supportedUris) {
+
+      val response: HttpResponse[String] = Http(s"${uri}").asString
+
+      assert(response.code == 415)
+
+      println(s"URI: ${uri}     MimeType: ${t}     Status: succesful")
+    }
+  }
+
+  test("notFound -> 404") {
+
+    for (uri <- nonExistentUris) {
+
+      val response: HttpResponse[String] = Http(s"${uri}").asString
+
+      assert(response.code == 404)
+
+      println(s"URI: ${uri}     Status: succesful")
+    }
   }
 
 }
